ff48ef94e496c0a6170b85a5238fa1d054d9fe5f
Philip Medlin <phmedlin@gmail.com>
2021-09-23 16:37:59 -0600

    Test: Add return character count test

--- spec/terminal_spec.rb
+++ spec/terminal_spec.rb
@@ -13,4 +13,8 @@ describe Terminal do
     expect(night_writer.output_braile_file).to eq "braille.txt"
   end
 
+  it 'returns the character count of user_provided_file' do
+    night_writer = Terminal.new("philips_message.txt", "braille.txt")
+    expect(night_writer.character_count).to eq 733
+  end
 end
