d4e9c76040fdcbf9739135f9ee5a1cad11d0ec74
Philip Medlin <phmedlin@gmail.com>
2021-09-23 16:23:56 -0600

    Test: add custom message test

--- spec/terminal_spec.rb
+++ spec/terminal_spec.rb
@@ -6,4 +6,11 @@ describe Terminal do
     night_writer = Terminal.new("message.txt", "braille.txt")
     expect(night_writer).to be_an_instance_of(Terminal)
   end
+
+  it 'returns a custom message to the command line' do
+    night_writer = Terminal.new("message.txt", "braille.txt")
+    expect(night_writer.user_provided_file).to eq "message.txt"
+    expect(night_writer.output_braile_file).to eq "braille.txt"
+  end
+
 end
