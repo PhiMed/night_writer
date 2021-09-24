1a47a0c7beabc8d87b6da1af7c11840b9cbdf73e
Philip Medlin <phmedlin@gmail.com>
2021-09-23 20:46:33 -0600

    Feat: Create Braille class and spec file

---
+++ spec/braille_spec.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+require './lib/terminal'
+require './lib/braille'
+
+describe Braille do
+  it 'exists' do
+    braille = Braille.new
+    expect(braille).to be_an_instance_of(Braille)
+  end
+
+  it 'XXXXXX' do
+
+  end
+
+  it 'XXXXX' do
+
+  end
+end
