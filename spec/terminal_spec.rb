71e116a856f3c3ac83169dda4c0c05123d6faa33
Philip Medlin <phmedlin@gmail.com>
2021-09-23 20:33:31 -0600

    Fix: Revise terminal spec test for cutsom message

--- spec/terminal_spec.rb
+++ spec/terminal_spec.rb
@@ -3,13 +3,13 @@ require './lib/terminal'
 
 describe Terminal do
   it 'exists' do
-    night_writer = Terminal.new("message.txt", "braille.txt")
+    night_writer = Terminal.new("philips_message.txt", "braille.txt")
     expect(night_writer).to be_an_instance_of(Terminal)
   end
 
   it 'returns a custom message to the command line' do
-    night_writer = Terminal.new("message.txt", "braille.txt")
-    expect(night_writer.user_provided_file).to eq "message.txt"
+    night_writer = Terminal.new("philips_message.txt", "braille.txt")
+    expect(night_writer.user_provided_file).to eq "philips_message.txt"
     expect(night_writer.output_braile_file).to eq "braille.txt"
   end
 
